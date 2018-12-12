722fc7aef136b299fdf650d90cbb2b1827ed270d
Evan Crook <evannc@gmail.com>
2018-12-12 14:55:26 -0800

    EC: skeleton of structure

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from db import DB
+
+def create_app():
+    app = Flask(__name__, static_url_path="/static")
+
+    @app.route("/users", methods=["POST"])
+    def signup():
+        pass
+
+    @app.route("/message", methods=["POST"])
+    def message_users():
+        # Send a message to all users
+        pass
+
+    @app.route("/response", methods=["POST"])
+    def handle_response():
+        # Webhook for the user texting us / twilio hits this
+        pass
+
+    DB.init_app(app)
+
+    return app
+
+if __name__ == "__main__":
+    create_app()
