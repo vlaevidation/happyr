372f196819ba490971c64564f9dc3d9a2558d35c
Evan Crook <evannc@gmail.com>
2018-12-12 15:06:15 -0800

    EC: serve a static page

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from app.db import DB
 
 def create_app():
     app = Flask(__name__, static_url_path="/static")
+
+    @app.route("/")
+    def index():
+        return render_template('index.html')
 
     @app.route("/users", methods=["POST"])
     def signup():
