bca43cfe872a5a17acc1c997556a37cd9800be7a
Evan Crook <ecrook@evidation.com>
2018-12-13 11:59:45 -0800

    EC: I am an idiot

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 def normalize_number(number):
     """Normalize a phone number str to format 18051234567"""
-    strip_chars = ['()-.+ ']
+    strip_chars = '()-.+ '
     for char in strip_chars:
         number = number.replace(char, '')
     if len(number) == 10:
